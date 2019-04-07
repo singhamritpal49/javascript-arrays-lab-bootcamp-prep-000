0f6c16e636364551551badeec24fd844b000d72e
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-07 04:13:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push("Ralph")
-
 }
-
+function destructivelyAppendKitten(name) {
+  kittens.slice(-1)
 
 
 
