3099b56f5f58276c324881ddc4a0fe4b06db9385
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-07 04:05:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+function destructivelyAppendKitten(name) {
+  kittens.push("meme")
+}
+
+
+
+
 
 // Add your functions and code here
